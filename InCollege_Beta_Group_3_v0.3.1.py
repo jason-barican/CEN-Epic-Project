ac2d4f24cc27a60a909ad1b2964b556c26353ef8
Jason Barican <jasonbaricanbusiness@gmail.com>
2023-02-21 19:34:46 -0500

    Added GUI functionality to 'Find Someone' and 'Sign-in'

--- InCollege_Beta_Group_3_v0.3.1.py
+++ InCollege_Beta_Group_3_v0.3.1.py
@@ -17,27 +17,33 @@ accPasswords = []
 accFullNames = []
 cont=True
 
+#presents menu for user to utilize application functions
 class OptionsWindow(tk.Frame):
   def __init__(self, parent, controller):
     tk.Frame.__init__(self, parent)
     self.controller = controller
     
+    accFile = open("accounts.txt","r+")
+    accounts = accFile.readlines()
+    self.controller.loadAccounts(accounts)
     
-    tk.Label(self, text = "Welcome to InCollege Beta!\n Please select an option below.").pack(padx = 10, pady = 10)
+    tk.Label(self, text = "Welcome to InCollege Beta!\n\nINCOLLEGE SUCCESS STORY\nJohn used LinkedIn.\nHe could not get a job.\nJohn then started using InCollege.\nHe got a job.\n\nPlease select an option below.").pack(padx = 10, pady = 10)
     self.pack(padx = 10, pady = 10)
 
     loginButton = tk.Button(self, text = "Login",
                             command = lambda: controller.show_frame("LoginWindow"))
     loginButton.pack(padx=10, pady=10)
 
-    signInButton = tk.Button(self, text = "Sign-In")
+    signInButton = tk.Button(self, text = "Sign-In",
+                            command = lambda: controller.show_frame("SignInWindow"))
     signInButton.pack(padx = 10, pady = 10)
 
     videoButton = tk.Button(self, text = "Play Video",
                             command = lambda: controller.show_frame("VideoWindow"))
     videoButton.pack(padx = 10, pady = 10)
 
-    findSomeoneButton = tk.Button(self, text = "Find Someone")
+    findSomeoneButton = tk.Button(self, text = "Find Someone",
+                            command = lambda: controller.show_frame("FindSomeoneFrame"))
     findSomeoneButton.pack(padx = 10, pady = 10)
 
     exitButton = tk.Button(self, text = "Exit", command = self.quit)
@@ -50,9 +56,7 @@ class LoginWindow(tk.Frame):
 
     tk.Label(self, text = "Please enter username and password.").pack(padx=10, pady=10)
 
-    accFile = open("accounts.txt","r+")
-    accounts = accFile.readlines()
-    self.loadAccounts(accounts)
+    
 
     usernameLabel = tk.Label(self, text = "Username:")
     usernameLabel.pack(padx=10, pady=10)
@@ -91,17 +95,124 @@ class LoginWindow(tk.Frame):
             self.controller.show_frame("ApplicationWindow")
           else:
             messagebox.showerror("Error", "Incorrect username/password. Please try again.")
-
-  def loadAccounts(self, accounts):
-    accUsernames.clear()
-    accPasswords.clear()
-    accFullNames.clear()
-    for acc in accounts:
-      accInfo = acc.split()
-      accUsernames.append(accInfo[0])
-      accPasswords.append(accInfo[1])
-      accFullNames.append(accInfo[2] + " " + accInfo[3])
     
+class SignInWindow(tk.Frame):
+  def __init__(self, parent, controller):
+    tk.Frame.__init__(self, parent)
+    self.controller = controller
+    
+    self.accFile = open("accounts.txt","r+")
+    accounts = self.accFile.readlines()
+    self.controller.loadAccounts(accounts)
+    
+    count = 0
+    for acc in accounts:
+      count+=1
+    
+    if count >= 5:
+      tk.Label(self, text="All permitted accounts have been created. Please come back later.").pack(padx=10, pady=10)
+    else:
+      
+      tk.Label(self, text="Create Username:").pack(padx=10, pady=10)
+      self.newUsernameEntry = tk.Entry(self,bd = 5)
+      self.newUsernameEntry.pack(padx=10, pady=10)
+
+      self.enterButton = tk.Button(self, text = "Enter", 
+                            command = lambda: self.usernameCheck())
+      self.enterButton.pack(padx = 10, pady = 10)
+      
+      self.backButton = tk.Button(self, text = "Back", 
+                          command = lambda: controller.show_frame("OptionsWindow"))
+      self.backButton.pack(padx=10, pady=10)
+        
+
+  def usernameCheck(self):
+    
+    self.newUsername = self.newUsernameEntry.get().replace(" ","")
+    valid = False
+    while valid is False:
+      if len(accUsernames) == 0:
+        valid = True
+        tk.Label(self,text= "Create Password:").pack(padx=10, pady=10)
+        self.enterButton.pack_forget()
+        self.backButton.pack_forget()
+        
+        self.newPasswordEntry = tk.Entry(self,bd = 5)
+        self.newPasswordEntry.pack(padx=10, pady=10)
+
+        self.enterButton = tk.Button(self, text = "Enter", 
+                          command = lambda: self.passwordCheck())
+        self.enterButton.pack(padx = 10, pady = 10)
+        #ensuring username is unique
+      else:
+        if self.newUsername in accUsernames:
+          messagebox.showerror("Error", "Account username already in use. Please try again.")
+          break
+        else:
+          valid = True
+          tk.Label(self,text= "Create Password:").pack(padx=10, pady=10)
+          self.enterButton.pack_forget()
+          self.backButton.pack_forget()
+          
+          self.newPasswordEntry = tk.Entry(self,bd = 5)
+          self.newPasswordEntry.pack(padx=10, pady=10)
+
+          self.enterButton = tk.Button(self, text = "Enter", 
+                            command = lambda: self.passwordCheck())
+          self.enterButton.pack(padx = 10, pady = 10)
+
+       
+  def passwordCheck(self):  
+    valid = False
+  
+    while valid is False:
+      hasDigit = False
+      hasNonAlphaNumeric = False
+      
+      self.newPassword = self.newPasswordEntry.get().replace(" ","")
+
+      #validation check for length and presence of upercase, special, and numeric character.
+      if len(self.newPassword) >= 8 and len(self.newPassword) <= 12 and self.newPassword.lower() != self.newPassword:
+        for char in self.newPassword:
+          if char.isdigit():
+            hasDigit = True
+          if not char.isalpha():
+            hasNonAlphaNumeric = True
+      if hasDigit is True and hasNonAlphaNumeric is True:
+        valid = True
+        tk.Label(self,text= "Enter your first and last name:").pack(padx=10, pady=10)
+        self.enterButton.pack_forget()
+        self.fullNameEntry = tk.Entry(self,bd = 5)
+        self.fullNameEntry.pack(padx=10, pady=10)
+
+        self.enterButton = tk.Button(self, text = "Enter", 
+                            command = lambda: self.fullName())
+        self.enterButton.pack(padx = 10, pady = 10)
+
+      else:
+        messagebox.showerror("Error", "Invalid Password. Try a better password.")
+        break
+
+  def fullName(self):
+    valid = False
+
+    while valid is False:
+      self.name = self.fullNameEntry.get()
+      nameCount = self.name.split()
+
+      if len(nameCount) != 2:
+        messagebox.showerror("Error", "Invalid format, Two names not found. Please try again.")
+        break
+
+      else:
+        valid = True
+        self.accFile.write(self.newUsername + " " + self.newPassword + " " + self.name + "\n")
+        self.accFile.close()
+        messagebox.showinfo("Account Created", "You have successfully created a new account.")
+        self.destroy()
+
+
+
 
 class VideoWindow(tk.Frame):
   def __init__(self, parent, controller):
@@ -126,7 +237,7 @@ class ApplicationWindow(tk.Frame):
     jobSearchButton.pack(padx = 10, pady = 10)
 
     findSomeoneButton = tk.Button(self, text = "Find Someone",
-                                  command = lambda: controller.show_frame("UnderConstruction"))
+                                  command = lambda: controller.show_frame("FindSomeoneFrame"))
     findSomeoneButton.pack(padx = 10, pady = 10)
 
     learnSkillButton = tk.Button(self, text = "Learn a new skill",
@@ -157,8 +268,6 @@ class UnderConstruction(tk.Frame):
           
           
 
-
-
 class MainWindow(tk.Tk):
   def __init__(self, *args, **kwargs):
     tk.Tk.__init__(self, *args, **kwargs)
@@ -169,7 +278,8 @@ class MainWindow(tk.Tk):
     mainframe.pack(padx = 10, pady = 10, fill = 'both', expand = 1)
     self.framelist = {}                 #dictionary for different pages
 
-    for F in (OptionsWindow, LoginWindow, VideoWindow, ApplicationWindow, UnderConstruction):
+    for F in (OptionsWindow, LoginWindow, SignInWindow,
+              VideoWindow, ApplicationWindow, FindSomeoneFrame, UnderConstruction):
       frame_name = F.__name__
       frame = F(parent = mainframe, controller = self)
       frame.grid(row = 0, column = 0, sticky = 'nsew')
@@ -177,95 +287,63 @@ class MainWindow(tk.Tk):
 
     self.show_frame("OptionsWindow")
 
+    accFile = open("accounts.txt","r+")
+    accounts = accFile.readlines()
+    self.loadAccounts(accounts)
+
+
+
   def show_frame(self, frame_name):
     frame = self.framelist[frame_name]
     frame.tkraise()
+
+  def loadAccounts(self, accounts):
+    accUsernames.clear()
+    accPasswords.clear()
+    accFullNames.clear()
+    for acc in accounts:
+      accInfo = acc.split()
+      accUsernames.append(accInfo[0])
+      accPasswords.append(accInfo[1])
+      accFullNames.append(accInfo[2] + " " + accInfo[3])
+
+
+
+class FindSomeoneFrame(tk.Frame):
+  def __init__(self, parent, controller):
+    tk.Frame.__init__(self, parent)
+    self.controller = controller 
+    self.create_widgets()
+
+  def create_widgets(self):
+      self.name_label = tk.Label(self, text="Enter full name of person:")
+      self.name_entry = tk.Entry(self)
+      self.name_label.grid(row=0, column=0, padx=5, pady=5)
+      self.name_entry.grid(row=0, column=1, padx=5, pady=5)
+
+      self.search_button = tk.Button(self, text="Search", command=self.search)
+      self.search_button.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+
+      backButton = tk.Button(self, text = "Back", 
+                            command = lambda: self.controller.show_frame("OptionsWindow"))
+      backButton.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+  def search(self):
+      searched_name = self.name_entry.get()
+
+      if searched_name not in accFullNames:
+          result_text = "They are not a part of the InCollege system yet."
+      else:
+          result_text = f"{searched_name} is part of the InCollege system. Login or sign up to join them!"
+      result_label = tk.Label(self, text=result_text)
+      result_label.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+      
 
   
 if __name__ == '__main__':
   window = MainWindow()
   window.mainloop()
 
-
-  
-
-def newAccount(accounts, accFile):
-  count = 0
-  for acc in accounts:
-    count += 1
-
-  #checking if amount of accounts has exceeded maximum
-  if count >= 5:
-    print("All permitted accounts have been created, please come back later\n")
-  else:
-    valid = False
-    while valid is False:
-      newUsername = input("Choose Username: ").replace(" ","")
-
-      if len(accUsernames) == 0:
-        valid = True
-      else:
-        for accUser in accUsernames:
-          #ensuring username is unique
-          if accUser == newUsername:
-            print("Account username already in use.")
-            valid = False
-            break
-          else:
-            valid = True
-
-    valid = False
-
-    while valid is False:
-      hasDigit = False
-      hasNonAlphaNumeric = False
-      password = input("Choose Password: ")
-      #validation check for length and presence of upercase, special, and numeric character.
-      if len(password) >= 8 and len(password) <= 12 and password.lower() != password:
-        for char in password:
-          if char.isdigit():
-            hasDigit = True
-          if not char.isalpha():
-            hasNonAlphaNumeric = True
-      if hasDigit is True and hasNonAlphaNumeric is True:
-        valid = True
-      else:
-        print("Invalid password. Try a better password.")
-
-    valid = False
-
-    while valid is False:
-      name = input("Enter your first and last name: ")
-      nameCount = name.split()
-      if len(nameCount) != 2:
-        print("Invalid format, Two names not found.")
-      else:
-        valid = True
-        
-    #storing account data in file.
-    accFile.write(newUsername + " " + password + " " + name + "\n")
-    print("Account registered successfully!\n")
-
-
-#presents menu for user to utilize application functions
-def application_menu():
-  cont = True
-  print("\nYou have successfully logged in\n--------------------------------------")
-  while(cont):
-    print("Type number to select option:\n\n0. Exit\n1. Job Search/internship\n2. Find Someone You Know\n3. Learn a new skill\n4. Post a new job")
-    menuSelect = input("")
-    if menuSelect == "1":
-      job_search()
-    elif menuSelect == "2":
-      find_someone()
-    elif menuSelect == "3":
-      learn_skills()
-    elif menuSelect == "4":
-      add_Job()
-    elif menuSelect == "0":
-      cont = False
-    else:
-      print("Invalid menu selection.")
 
 #currently in progress job function
 def job_search():
